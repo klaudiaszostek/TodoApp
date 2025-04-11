ebaf8eec3b3e99fbafed0218d599de63697da4cd
Klaudia Szostek <kszostek@student.agh.edu.pl>
2025-04-11 11:02:56 +0200

    Version with login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app.app import db
 from datetime import datetime
 import pytz
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,9 +12,22 @@ class Task(db.Model):
     is_done = db.Column(db.Boolean, default=False)
     priority = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(pytz.timezone('Europe/Warsaw')))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500))
     author = db.Column(db.String(100))
     timestamp = db.Column(db.DateTime, default=db.func.now())
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    tasks = db.relationship('Task', backref='owner', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
